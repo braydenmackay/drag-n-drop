80ad22c2d1349b969910ef892099cd1050607777
James <55603199+James-Holiday@users.noreply.github.com>
2019-10-24 16:19:06 -0600

    Style changes

---
+++ TeamList.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Droppable } from "react-beautiful-dnd";
+
+import StudentDraggable from "./StudentDraggable";
+
+const TeamList = props => {
+  const [students] = React.useState(props.students);
+
+  const renderStudents = () => {
+    const teamPride = students.filter(
+      student => student.team === +props.number
+    );
+    return teamPride.map((student, index) => {
+      return (
+        <StudentDraggable key={student.id} student={student} index={index} />
+      );
+    });
+  };
+
+  return (
+    <Droppable droppableId={props.number}>
+      {provided => (
+        <div
+          className="team-list"
+          ref={provided.innerRef}
+          {...provided.droppableProps}
+        >
+          <div className="team-title">
+            <p>Team {props.number}</p>
+          </div>
+          <div className="team-students">{renderStudents()}</div>
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
+  );
+};
+
+export default TeamList;
